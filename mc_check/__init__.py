cfacce0e62276177d51a1422bc1c9b6834dd407c
molanp <molanp>
2024-12-01 22:35:44 +0800

    修复颜色渲染错误

--- mc_check/__init__.py
+++ mc_check/__init__.py
@@ -33,7 +33,7 @@ __plugin_meta__ = PluginMetadata(
     """.strip(),
     extra=PluginExtraData(
         author="molanp",
-        version="1.20",
+        version="1.21",
         limits=[PluginCdBlock(result=None)],
         menu_type="一些工具",
         configs=[
